cbe6d08a3f3d02488b5b6025f96294667008bb63
Nicolas Seriot <nicolas@seriot.ch>
2014-05-26 13:12:54 +0200

    added code for solution 'GENETIC1'

--- antennas_assignment.py
+++ antennas_assignment.py
@@ -9,6 +9,9 @@ import png # $ sudo pip install pypng, https://github.com/drj11/pypng
 
 #random.seed(0)
 
+SOLUTION = "RANDOM"
+#SOLUTION = "GENETIC1"
+
 def read_img_grid(image_path):
     f = open(image_path)
     img = png.Reader(file=f).read()
@@ -102,8 +105,66 @@ def write_to_json(o, path):
     f.write(s)
     f.close()
 
+def mutate_solution(antennas, pop_grid, cost_grid, cov_cost_grid, initial_locations):
+    # reallocate all antennas on nearly empty cells
+    
+    width, height = len(pop_grid[0]), len(pop_grid)
+
+    new_solution = []
+
+    nb_moves = 0
+
+    for (t,x,y) in initial_locations:
+        
+        (t_, x_, y_) = (t,x,y)
+        
+        if pop_grid[y][x] < 0.6:
+            nb_moves = nb_moves + 1
+            (t_, x_, y_) = (t, randint(0, width-1), randint(0, height-1))
+        
+        if cov_cost_grid and cov_cost_grid[y_][x_] is None:
+            new_solution.append( [t_, x_, y_] )
+        else:
+            new_solution.append( [t, x, y] )
+    #print "-- nb_moves:", nb_moves
+    
+    if nb_moves == 0:
+        print "- mutate"
+        # mutate 200 random antennas
+        for xxx in range(100):
+            i = randint(0, len(new_solution)-1)
+            
+            x_ = randint(0, width-1)
+
+            new_solution[i][1] = x_
+            new_solution[i][2] = y_
+            
+    return new_solution
+
 def compute_best_solution(antennas, pop_grid, cost_grid, budget):
-    return compute_random_antenna_locations(antennas, pop_grid)
+
+    if SOLUTION is 'RANDOM':
+        return compute_random_antenna_locations(antennas, pop_grid)
+    elif SOLUTION is 'GENETIC1':
+        solution = compute_random_antenna_locations(antennas, pop_grid)
+        people_covered = 0
+        cost = 0
+        
+        cov_cost_grid = None
+        
+        for i in range(100):
+            new_solution = mutate_solution(antennas, pop_grid, cost_grid, cov_cost_grid, solution)
+            (new_people_covered, new_cost, cov_cost_grid) = evaluate_solution(antennas, pop_grid, cost_grid, new_solution)
+            
+            if new_people_covered > people_covered and new_cost <= budget:
+                people_covered = new_people_covered
+                solution = new_solution
+                cost = new_cost
+                print "[%5d] best_people_covered: %d (%.02f%%), cost %f" % (i, people_covered, (100.0 * people_covered / total_pop), cost)
+    
+        return solution
+    else:
+        return None
 
 if __name__ == "__main__":
     
